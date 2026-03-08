57bf6ffebc2232b851588ee3e47c9b8607b6e657
Sezan00 <sezanvay.bd@gmail.com>
2026-03-08 20:44:05 +0600

    done

--- src/Api/RegisterApi.jsx
+++ src/Api/RegisterApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "https://bnpl-backend.sezan.xyz/api/api";
+const API_URL = "https://bnpl-backend.sezan.xyz/api";
 
 export const registerUser = async (formData) => {
     const response = await axios.post(`${API_URL}/register`, formData);
